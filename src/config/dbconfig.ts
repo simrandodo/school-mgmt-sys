055adcb90fa712b78dd94698cc33db8d1e7fd7f7
vanshika-1012 <avanshika299@gmail.com>
2024-02-16 11:15:29 +0530

    updated db config file

---
+++ src/config/dbconfig.ts
@@ -0,0 +1,65 @@
+import { Injectable, Scope } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { isNil } from 'lodash';
+
+@Injectable({ scope: Scope.DEFAULT })
+export class DbConfig {
+  //prodSchemas: Map<string, string> = new Map();
+
+  constructor(private readonly configService: ConfigService) {}
+
+  private get(key: string): string {
+    const value = this.configService.get(key);
+
+    if (isNil(value)) {
+      throw new Error(key + ' environment variable does not set'); // probably we should call process.exit() too to avoid locking the service
+    }
+    return value;
+  }
+
+  private getString(key: string): string {
+    return this.get(key).trim();
+  }
+
+  private getNumber(key: string): number {
+    const value = this.get(key);
+
+    try {
+      return parseInt(value);
+    } catch {
+      throw new Error(key + ' environment variable is not a number');
+    }
+  }
+
+  private getBoolean(key: string): boolean {
+    const value = this.get(key);
+
+    try {
+      return Boolean(JSON.parse(value));
+    } catch {
+      throw new Error(key + ' env var is not a boolean');
+    }
+  }
+
+  get isSyncronization(): boolean {
+    return this.getBoolean('IS_SYNCRONIZATION');
+  }
+
+  getPostGresConfig(): TypeOrmModuleOptions {
+    //const migrations = ['dist/src/migrations/*{.ts, .js}'];
+    return {
+      type: 'postgres',
+     // migrations,
+      host: this.getString('DB_HOST'),
+      port: this.getNumber('DB_PORT'),
+      username: this.getString('DB_USERNAME'),
+      password: this.getString('DB_PASSWORD'),
+      database: this.getString('DB_NAME'),
+      //logging: this.getBoolean('ENABLE_ORM_LOGS_REDSHIFT'),
+      autoLoadEntities: true,
+      synchronize: this.isSyncronization,
+      poolSize: 20,
+    };
+  }
+}
